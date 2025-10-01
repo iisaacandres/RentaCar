90ac76317f3bfa9e6e6483392ab41a21c16037ef
iisaacandres <isaacgonzalez390@gmail.com>
2025-10-01 00:57:48 -0400

    me envole

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from mainApp.models import Auto, Reserva, Boleta
 
-# Register your models here.
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ['marca', 'modelo', 'anio', 'patente', 'precio_por_dia', 'color']
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ['auto', 'cliente_nombre', 'cliente_email', 'fecha_inicio', 'fecha_fin', 'codigo_descuento']
+
+class BoletaAdmin(admin.ModelAdmin):
+    list_display = ['numero_boleta', 'reserva', 'fecha_pago', 'total_original', 'total_descuento', 'total_final']
+
+
+admin.site.register(Auto, AutoAdmin)
+admin.site.register(Reserva, ReservaAdmin)
+admin.site.register(Boleta, BoletaAdmin)
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from mainApp.models import Auto, Reserva, Boleta
+from mainApp.forms import FormReserva
+import datetime
 
-# Create your views here.
+DESCUENTO = 20  
 
-def autos(request):
-    return
+def index(request):
+    autos = Auto.objects.all()
+    data = {'autos': autos}
+    return render(request, 'index.html', data)
+
+
+def ticket(request, id):
+    boleta = Boleta.objects.get(id=id)
+    data = {'boleta': boleta}
+    return render(request, 'ticket.html', data)
+
+
+def reservar(request, id):
+    form = FormReserva()
+    form.fields['id_auto'].initial = id
+
+    if request.method == 'POST':
+        form = FormReserva(request.POST)
+        if form.is_valid():
+            auto = Auto.objects.get(id=id)
+
+            fecha_inicio = form.cleaned_data['fecha_inicio']
+            fecha_fin = form.cleaned_data['fecha_fin']
+            dias = (fecha_fin - fecha_inicio).days + 1
+            if dias < 1:
+                dias = 1
+
+            reserva = Reserva()
+            reserva.auto = auto
+            reserva.cliente_nombre = form.cleaned_data['cliente_nombre']
+            reserva.cliente_email = form.cleaned_data['cliente_email']
+            reserva.fecha_inicio = fecha_inicio
+            reserva.fecha_fin = fecha_fin
+            reserva.codigo_descuento = form.cleaned_data['codigo_descuento']
+            reserva.save()
+
+
+            total_original = auto.precio_por_dia * dias
+            descuento = DESCUENTO if reserva.codigo_descuento == datetime.date.today().strftime("%Y-%m-%d") else 0
+            total_descuento = (total_original * descuento) / 100
+            total_final = total_original - total_descuento
+
+            boleta = Boleta()
+            boleta.reserva = reserva
+            boleta.numero_boleta = f"B-{reserva.id:05d}"
+            boleta.fecha_pago = datetime.date.today()
+            boleta.total_original = total_original
+            boleta.total_descuento = total_descuento
+            boleta.total_final = total_final
+            boleta.save()
+
+            return redirect('/ticket/' + str(boleta.id))
+
+    auto = Auto.objects.get(id=id)
+    data = {
+        'auto': auto,
+        'form': form
+    }
+    return render(request, 'reservar.html', data)
